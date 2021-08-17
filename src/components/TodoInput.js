8510fda9d020a60b9b343dd28d49ef763a5fd8bf
pdo9 <do.polyanin@gmail.com>
2021-08-17 11:36:23 +0700

    Added input form - some fixes

---
+++ src/components/TodoInput.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function TodoInpunt(props) {
+  let [todoInputValue, setTodoInputValue] = React.useState('');
+  //todoInputValue = 'Введите текст заметки';
+
+  function submitEventHandler(event) {
+    event.preventDefault();
+
+    if (todoInputValue.trim()) {
+      props.onCreate(todoInputValue);
+      setTodoInputValue('');
+    }
+  }
+
+  return (
+    <form
+      className='todo-input'
+      onSubmit={submitEventHandler}
+      style={{ margin: '10px 0px' }}
+    >
+      <input
+        value={todoInputValue}
+        onChange={(event) => setTodoInputValue(event.target.value)}
+      />
+      <button type='submit' style={{ margin: '10px' }}>
+        Добавить
+      </button>
+    </form>
+  );
+}
