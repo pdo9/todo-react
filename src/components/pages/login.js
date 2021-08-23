d7d0d95191947edaedcc7357a799f1e1016ccb31
pdo9 <do.polyanin@gmail.com>
2021-08-23 14:32:01 +0700

    Add routing

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -4,6 +4,7 @@ export const Login = () => {
   return (
     <Fragment>
       <h1>Авторизация</h1>
+      <button style={{ height: '40px', width: '100px' }}>Вход</button>
     </Fragment>
   );
 };
